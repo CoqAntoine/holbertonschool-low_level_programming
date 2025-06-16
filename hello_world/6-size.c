1185d559333ecbf5b13caa747bbb37781abccace
CoqAntoine <11534@holbertonstudents.com>
2025-06-16 12:40:57 +0000

    task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+    * main - Entry point of the program
+    *
+    * This program prints "Programming is like building a multilingual puzzle",
+    * followed by a new line.
+    *
+    * Return: Always 0 (Success)
+    */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
+	return (0);
+}
