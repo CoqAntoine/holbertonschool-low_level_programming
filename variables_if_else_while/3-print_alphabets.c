f94843a1039b6f2e58cfa206a957c4ad9d44fa98
CoqAntoine <11534@holbertonstudents.com>
2025-06-17 09:03:10 +0000

    task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		printf("%c", c);
+	for (c = 'A'; c <= 'Z'; ++c)
+		printf("%c", c);
+	printf("\n");
+	return (0);
+}
