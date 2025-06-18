56e859ebd87a384a8a1456b5ad5af142a456c464
CoqAntoine <11534@holbertonstudents.com>
2025-06-18 13:44:00 +0000

    task 1

---
+++ functions_nested_loops/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
