0c5c40dbc29084d8b43592e01f5e9125b3817d55
CoqAntoine <11534@holbertonstudents.com>
2025-06-18 13:50:25 +0000

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - print the alphabet
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 }
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,19 +1,13 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_alphabet - print the alphabet
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+int main(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	putchar('\n');
+	print_alphabet();
+	return (0);
 }
