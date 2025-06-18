e10f56695be8ee4c3a573aac0ede6d530b8fa44f
CoqAntoine <11534@holbertonstudents.com>
2025-06-18 14:57:38 +0000

    task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+	        return (1);
+	else
+		return (0);
+}
