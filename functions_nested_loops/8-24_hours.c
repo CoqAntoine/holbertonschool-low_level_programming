f7479085fd565bb44d7b30e70ccd31912515b1cb
CoqAntoine <11534@holbertonstudents.com>
2025-06-20 08:49:03 +0000

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -23,9 +23,7 @@ void jack_bauer(void)
 				{
 					_putchar('0' + i);
 					_putchar('0' + j);
-					_putchar(' ');
 					_putchar(':');
-					_putchar(' ');
 					_putchar('0' + k);
 					_putchar('0' + l);
 					_putchar('\n');
