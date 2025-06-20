3ac425db662e6ad5b2db5b262b5f0c86376987c1
CoqAntoine <11534@holbertonstudents.com>
2025-06-20 09:42:46 +0000

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - print the alphabet
+ *
+ * Return: Always 0.
+ */
+
+int add(int int1, int int2)
+{
+	int addition = 0;
+
+	addition = int1 + int2;
+	return (addition);
+}
