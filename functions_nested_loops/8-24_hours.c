ed318fe736d1676a4f186cac22ee566ead9351c1
CoqAntoine <11534@holbertonstudents.com>
2025-06-20 08:47:53 +0000

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - print the alphabet
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l = 0;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i == 2 && j == 4)
+				break;
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(' ');
+					_putchar(':');
+					_putchar(' ');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
