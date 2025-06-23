774afdf3c01e155a6973747548f204e5ca27a6e3
CoqAntoine <11534@holbertonstudents.com>
2025-06-23 08:32:20 +0000

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ *
+ * @a : entier a verifier
+ *
+ * @b : entier a verifier
+ *
+ * Return: retourne 1 si digit, 0 sinon
+ */
+
+int mul(int a, int b)
+{
+	int multiple = a * b;
+
+	return (multiple);
+}
