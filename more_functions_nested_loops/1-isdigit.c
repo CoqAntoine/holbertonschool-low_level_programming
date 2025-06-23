41365e3e1179e11cb1cd995c0db9f8479a58f343
CoqAntoine <11534@holbertonstudents.com>
2025-06-23 08:25:55 +0000

    task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - check the code
+ *
+ * @c : entier a verifier
+ *
+ * Return: retourne 1 si digit, 0 sinon
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '1')
+		return (1);
+	else
+		return (0);
+}
