4ee2a7ddc894bd19b411b0207b4c5d4ec16516cc
CoqAntoine <11534@holbertonstudents.com>
2025-06-23 08:46:00 +0000

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: retourne 1 si digit, 0 sinon
+ */
+
+void print_most_numbers(void)
+{
+	char c = 0;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '2' || c == '4')
+			break;
+		_putchar(c);
+	}
+	_putchar('\n');
+}
