1b98f1b48038c86e852914b7efb44145a73ad880
CoqAntoine <11534@holbertonstudents.com>
2025-06-24 13:11:38 +0000

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _print_rev - check the code
+ *
+ * @s : le string qui doit etre reverse
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int compteur = 0;
+
+	while (s[compteur] != '\0')
+	{
+		compteur++;
+	}
+
+	compteur--;
+
+	for (; compteur >= 0; compteur--)
+		_putchar(s[compteur]);
+	_putchar('\n');
+}
