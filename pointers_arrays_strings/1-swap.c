a172ed885ba4d8f7ef45a776bcffc7ec54dd195e
CoqAntoine <11534@holbertonstudents.com>
2025-06-24 09:18:29 +0000

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ *
+ * @a : pointeur a
+ *
+ * @b : pointeur b
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c = *a;
+	*a = *b;
+	*b = c;
+}
