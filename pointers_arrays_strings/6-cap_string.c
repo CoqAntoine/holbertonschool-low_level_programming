dcee0dbb8ad5654f40a537d174525fea0a47d439
CoqAntoine <11534@holbertonstudents.com>
2025-06-27 12:19:09 +0000

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - check the code
+ *
+ * @s : string input
+ *
+ * Return: string with all word capitalized
+ */
+
+char *cap_string(char *s)
+{
+	int cpt = 0;
+	int cpt2 = 0;
+char c[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[cpt])
+	{
+		cpt2 = 0;
+		while (c[cpt2])
+		{
+			if (s[cpt] == sep[cpt2] && s[cpt + 1] >= 'a' && s[cpt + 1] <= 'z')
+			{
+				s[cpt + 1] = s[cpt + 1] - 32;
+			}
+			cpt2++;
+		}
+		cpt++;
+	}
+	return (s);
+}
