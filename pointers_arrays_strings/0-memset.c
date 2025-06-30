ae6d68bda73a334ce8a3f3dea5434ed0ab462953
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 <11534@holbertonstudents.com>
2025-06-30 14:25:53 +0200

    task 0. memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - prints buffer in hexa
+ * @s: the address of memory to print
+ * @a: the size of the memory to print
+ * @b: the constant byte
+ *
+ * Return: pointer s
+ */
+
+ char *_memset(char *s, char b, unsigned int n)
+ {
+    unsigned int cpt = 0;
+    unsigned int i = 0;
+
+    while (s[cpt])
+    {
+        cpt++;
+    }
+
+    for (i = 0; i <= n; i++)
+    {
+        if (i == cpt)
+            break;
+        s[i] = b;
+    }
+    return (s);
+ }
