e739cadca634500aaa799c2cdc695695eb669527
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 <11534@holbertonstudents.com>
2025-06-30 15:52:13 +0200

    task 2. strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - prints buffer in hexa
+ * @s: string to read
+ * @c: character to find
+ *
+ * Return: pointer that point to the first occurence of the character c
+ */
+
+char *_strchr(char *s, char c)
+{
+unsigned int i = 0;
+char *ptr;
+
+for (i = 0; s[i]; i++)
+{
+if (s[i] == c)
+{
+ptr = &s[i];
+break;
+}
+else if (s[i] == '\0')
+return (NULL);
+}
+return (ptr);
+}
