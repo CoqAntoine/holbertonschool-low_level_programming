84357f0ac4b17e5aa139212be65d03eef1e72dc7
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 <11534@holbertonstudents.com>
2025-07-01 10:44:55 +0200

    task 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -13,20 +13,17 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-char *ptr;
 int i, j;
 
 for (i = 0; haystack[i]; i++)
 {
 for (j = 0; needle[j]; j++)
 {
-if (haystack[i] == needle[0])
-{
-ptr = &haystack[i];
-if (haystack[i] == needle[j])
-return (ptr);
+if (haystack[i + j] != needle[j])
+break;
 }
-}
+if (needle[j] == '\0')
+return (&haystack[i]);
 }
 return (NULL);
 }
