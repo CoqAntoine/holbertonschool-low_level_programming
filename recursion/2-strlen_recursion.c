67a7533a9f95f289ce11ccb8f9683b92b9fbb6b8
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 <11534@holbertonstudents.com>
2025-07-01 14:27:39 +0200

    task 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - check the code
+ * @s: string to print in reverse
+ *
+ * Return: lenght of s
+ */
+
+int _strlen_recursion(char *s)
+{
+if (s[0] == '\0')
+{
+return (0);
+}
+else
+return (*1 + _strlen_recursion(s + 1));
+}
