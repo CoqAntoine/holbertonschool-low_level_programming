577e4b52d8f2afdf3001be1103d55148c8776788
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 <11534@holbertonstudents.com>
2025-07-01 10:39:06 +0200

    task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - check the code
+ *
+ * @haystack : string de reference
+ *
+ * @needle : string a chercher
+ *
+ * Return: pointer to haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+char *ptr;
+int i, j;
+
+for (i = 0; haystack[i]; i++)
+{
+for (j = 0; needle[j]; j++)
+{
+if (haystack[i] == needle[0])
+{
+ptr = &haystack[i];
+return (ptr);
+}
+}
+}
+return (NULL);
+}
