b8d62bd19cb67c05f26d47471069ee3cb824facb
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 <11534@holbertonstudents.com>
2025-07-02 11:01:18 +0200

    betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - check the code.
+ * @argc: entier
+ * @argv: array
+ * Return: Always 0.
+ */
+
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[argc]);
-return (0);
+printf("%s\n", argv[0]);
+return (argc - 1);
 }
