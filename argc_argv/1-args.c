3d531860c7c43150a25472d5e1708b54c9dad8ce
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 <11534@holbertonstudents.com>
2025-07-02 11:04:46 +0200

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * @argc: entier
+ * @argv: array
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+argv[0] = 0;
+return (0);
+}
