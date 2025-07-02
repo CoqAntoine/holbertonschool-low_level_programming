65fc49e1f70f2571f2e0819c187ea396d312d6dc
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 <11534@holbertonstudents.com>
2025-07-02 10:21:41 +0200

    task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
- * checker - 
- * @square: 
- * @n: 
- * Return: 
+ * checker - Recursively checks if a number is divisible by any number
+ *           from 'square' up to the square root of 'n'.
+ * @square: the current divisor being tested
+ * @n: the number to check for primality
+ *
+ * Return: 1 if 'n' is a prime number (no divisors found),
+ *         0 if 'n' is not a prime number (divisor found)
  */
 
 int checker(int square, int n)
@@ -19,9 +22,10 @@ int checker(int square, int n)
 }
 
 /**
- * is_prime_number - 
- * @n: 
- * Return: 
+ * is_prime_number - Determines whether a given integer is a prime number.
+ * @n: the number to check
+ *
+ * Return: 1 if 'n' is a prime number, 0 otherwise
  */
 
 int is_prime_number(int n)
