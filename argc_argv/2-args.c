41268ac302117dc45a4601b604a8605a57ad0a05
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 <11534@holbertonstudents.com>
2025-07-02 11:16:21 +0200

    task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * @argc: entier
+ * @argv: array
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+while(argc--)
+printf("%s\n", *argv++);
+return (0);
+}
