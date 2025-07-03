32b5e9d197b642884e5dfd4ecc1a6a57530d87b7
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 <11534@holbertonstudents.com>
2025-07-03 13:58:01 +0200

    task 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,14 +8,27 @@
  * Return: Always 0.
  */
 
+int is_numeric(char *n)
+{
+	if (n[0] >= '0' && n[0] <= '9')
+		return (0);
+	return (1);
+}
+
 int main(int argc, char *argv[])
 {
 	int sum = 0;
 	int cpt = 1;
 
+	argc--;
 	while (argc--)
 	{
-		sum += atoi(argv[cpt]);
+		if (is_numeric(argv[cpt]) == 1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum = sum + (atoi(argv[cpt]));
 		cpt++;
 	}
 	printf("%d\n", sum);
