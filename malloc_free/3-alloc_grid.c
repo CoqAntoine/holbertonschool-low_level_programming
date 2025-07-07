c51e125c70684011b29b06e22a8d5dce7ea682a6
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 <11534@holbertonstudents.com>
2025-07-07 11:48:38 +0200

    betty change

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -31,7 +31,7 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
-	for (j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
 	return (grid);
