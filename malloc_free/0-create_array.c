8404e865e1bda8cbef711c4e7369fd04bf699289
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 <11534@holbertonstudents.com>
2025-07-07 14:27:39 +0200

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,12 +14,15 @@ char *create_array(unsigned int size, char c)
 	char *str;
 
 	if (size == 0)
-	return (NULL);
+		return (NULL);
+
 	str = malloc(sizeof(char) * size);
 
 	if (str == NULL)
-	return (NULL);
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		str[i] = c;
+
 	return (str);
 }
