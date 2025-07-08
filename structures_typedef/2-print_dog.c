be72609d17a3dcd24f21ca51e6fde5aea4b54a1c
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 <11534@holbertonstudents.com>
2025-07-08 15:49:43 +0200

    task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\nAge: %f\nOwner: %s", )
+	if (d.name == NULL)
+		printf("(nil)\n")
+	else
+		printf("%s\n", d.name)
+	if (d.age == NULL)
+		printf("(nil)\n")
+	if (d.owner == NULL)
+		printf("(nil)\n")
+
+}
