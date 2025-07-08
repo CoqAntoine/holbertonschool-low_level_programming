7eeb029803f59cb1abde13fe85ce7c36c09b8ccc
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 <11534@holbertonstudents.com>
2025-07-08 10:57:25 +0200

    robustesse

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Structure defining a dog
+ * @name: Pointer to the dog's name
+ * @age: Dog's age (in float)
+ * @owner: Pointer to the owner's name
+ *
+ * Description: A structure that stores information about a dog,
+ * including its name, age, and the name of its owner.
+ */
+
 struct dog 
 {
     char *name;
