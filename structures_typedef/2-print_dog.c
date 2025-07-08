bf06953a1c87fc978c881840192ee241b5d1cada
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 <11534@holbertonstudents.com>
2025-07-08 15:57:35 +0200

    betty

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * print_dog - Prints a struct dog, handling NULL fields gracefully
+ * @d: Pointer to struct dog to print
+ */
+
+void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
 
 	printf("Name: ");
-	if (d.name == NULL)
+	if (d->name == NULL)
 		printf("(nil)\n");
 	else
-		printf("%s\n", d.name);
-	printf("Age: ")
-	if (d.age == NULL)
+		printf("%s\n", d->name);
+	printf("Age: ");
+	if (d->age == 0.0)
 		printf("(nil)\n");
 	else
-		printf("%f\n", d.age);
+		printf("%f\n", d->age);
 	printf("Owner: ");
-	if (d.owner == NULL)
+	if (d->owner == NULL)
 		printf("(nil)\n");
 	else
-		printf("%s\n", d.age);
+		printf("%s\n", d->owner);
 
 }
