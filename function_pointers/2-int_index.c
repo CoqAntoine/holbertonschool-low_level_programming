6ea05322747e6095e94321c9a8348d02f04a8948
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 <11534@holbertonstudents.com>
2025-07-15 15:03:26 +0200

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -16,10 +16,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0 || size > INT_MAX)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
