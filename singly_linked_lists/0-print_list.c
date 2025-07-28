3c550e08cdb0d734288962adb230e4b25886026b
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 <11534@holbertonstudents.com>
2025-07-28 11:10:03 +0200

    delete _putchar

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,18 +9,6 @@
  *
  * Return: The number of nodes printed
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
-/**
- * print_list - Prints all the elements of a list_t linked list
- * @h: Pointer to the head of the list
- *
- * Return: The number of nodes printed
- */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0, j;
