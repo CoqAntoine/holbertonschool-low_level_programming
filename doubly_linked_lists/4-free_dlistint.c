de2d73fd37987bd7eaca3d5eb4e49fa3cbd62c63
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 <11534@holbertonstudents.com>
2025-07-29 11:23:02 +0200

    fix betty issue

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * free_dlistint - Frees a dlistint_t list
+ * @head: Pointer to the head of the list
+ *
+ * Return: Nothing
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
