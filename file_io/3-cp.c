b2e82b9bae07c78b69b1a1654654a2306bdffc3f
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 <11534@holbertonstudents.com>
2025-08-01 11:28:26 +0200

    fix 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -33,7 +33,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
@@ -53,6 +52,7 @@ int main(int argc, char *argv[])
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
 		}
 	}
+	read_count = read(fd_from, buffer, 1024))
 	if (read_count == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -62,6 +62,5 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-
 	return (0);
 }
